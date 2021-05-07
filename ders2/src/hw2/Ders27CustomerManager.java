b3bcd73ea277daa926884cb12e1088f202e300f0
gokalpclk <06gokalp@gmail.com>
2021-05-07 03:55:02 +0300

    lecture2-homework2

---
+++ ders2/src/hw2/Ders27CustomerManager.java
@@ -0,0 +1,15 @@
+package hw2;
+
+public class Ders27CustomerManager {
+	public void Add() {
+		System.out.println("Musteri eklendi");;
+	}
+	
+	public void Remove() {
+		System.out.println("Musteri silindi");
+	}
+	
+	public void Update() {
+		System.out.println("musteri guncellendi");;
+	}
+}
---
+++ ders2/src/hw2/Ders31ProductManager.java
@@ -0,0 +1,12 @@
+package hw2;
+
+public class Ders31ProductManager {
+	public void Add(Ders31Product ders31Product) {
+		System.out.println("Urun eklendi " + ders31Product.getName());
+	}
+	
+	public void Add2(int id, String name, String description, int stockAmount, double price) {
+		
+	}
+
+}
